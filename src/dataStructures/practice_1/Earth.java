aa7f1bf5764306eae2c938bb11d3166d1450e974
Mario Medrano Maldonado <mariomedranomaldonado@gmail.com>
2018-03-14 00:00:14 -0400

    Practice 1 for data structures course.

---
+++ src/dataStructures/practice_1/Earth.java
@@ -0,0 +1,14 @@
+package dataStructures.practice_1;
+
+/**
+ * Created by mario on 3/13/2018.
+ */
+
+public class Earth {
+
+    public static void main(String[] args) {
+        Human one = new Human();
+        one.eat();
+        one.speak();
+    }
+}
---
+++ src/dataStructures/practice_1/Human.java
@@ -0,0 +1,28 @@
+package dataStructures.practice_1;
+
+/**
+ * Created by mario on 3/13/2018.
+ */
+public class Human {
+
+    // Properties
+
+    private String name;
+    private int age;
+    private int heightInInches;
+    private String eyeColor;
+
+    // Constructor
+    public Human () {
+
+    }
+
+    // Instructions
+
+    public void speak () {
+        System.out.println("My name is: " + this.name);
+    }
+    public void eat () {
+        System.out.println("I am eating");
+    }
+}
